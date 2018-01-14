10dd4aa92f7f288e4597dc131cecb85fce373893
jahai3 <35411558+jahai3@users.noreply.github.com>
2018-01-14 20:51:03 +1100

    Update Control_Arm_PWM_Board.ino

--- Control_Arm_PWM_Board/Control_Arm_PWM_Board.ino
+++ Control_Arm_PWM_Board/Control_Arm_PWM_Board.ino
@@ -1,7 +1,15 @@
+/* **************************************************************************************
+ *  NOVA ROVER TEAM - URC2018
+ *  This code controls the digital signals, PWM and linear actuators with a PWM feedback loop.
+ *
+ *Author: James Hain
+ ****************************************************************************************/
+
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <math.h>
 
+// PWM board initialisation
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 #define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
@@ -50,11 +58,12 @@ int speed = 2048;   // out of 4095
                
 
 
-//Angle read from potentiometers
+//Variables for angle read from potentiometers
 int lowerReadAngle = 0;
 int upperReadAngle = 0;
 
 
+// Setup function for pins, serial for debug and PWM
 void setup()
 {
   //  setup serial
@@ -79,12 +88,13 @@ void setup()
   }
 }
 
+// Function to decode motor direction, returns 1 if positive number, 0 otherwise
 static inline int8_t isPositive(int val) {
- if (val < 0) return 0;
- if (val==0) return 0;
+ if (val <= 0) return 0;
  return 1;
 }
 
+//Function sets pulse width for servo if direct PWM instruction not wanted
 void setServoPulse(uint8_t n, double pulse) {
   double pulselength;
   
@@ -99,17 +109,15 @@ void setServoPulse(uint8_t n, double pulse) {
   pwm.setPWM(n, 0, pulse);
 }
 
+// Function sets the lower linear actuator position given potentiometer voltage for this actuator's range of movement
 void actuatorLower(int angle, int speed)
 {
   
-  lowerReadAngle = map(analogRead(lowerPin),291,555,148,77);
+  lowerReadAngle = map(analogRead(lowerPin),291,555,148,77);        // Read PWM voltage and scale to joint angle
   
-
-
-  
-  while(lowerReadAngle != angle)
+  while(lowerReadAngle != angle)                                    // While the read angle isn't the wanted angle
   {
-    if (lowerReadAngle > angle)
+    if (lowerReadAngle > angle)                                     // Extend or retract the actuator accordingly
     {
       pwm.setPWM(lowerPWMPin, 0, speed);
       digitalWrite(directionLowerPin, HIGH);
@@ -119,27 +127,24 @@ void actuatorLower(int angle, int speed)
       pwm.setPWM(lowerPWMPin, 0, speed);
       digitalWrite(directionLowerPin, LOW);
     }
-    
-    
+   
     //Serial.println("Actuator Lower");
     //Serial.println(val1);
     lowerReadAngle = map(analogRead(lowerPin),291,555,148,77);
   }
-  pwm.setPWM(lowerPWMPin, 0, 0);
+  pwm.setPWM(lowerPWMPin, 0, 0);                                    // Stop movement
   
 }
 
+// Function sets the upper linear actuator position given potentiometer voltage for this actuator's range of movement
 void actuatorUpper(int angle, int speed)
 {
-  upperReadAngle = map(analogRead(upperPin),95,406,156,75);
+  upperReadAngle = map(analogRead(upperPin),95,406,156,75);              // Read PWM voltage and scale to joint angle
   
-
-
-  
-  while(upperReadAngle != angle)
+  while(upperReadAngle != angle)                                         // While the read angle isn't the wanted angle
   {
 
-    if (upperReadAngle > angle)
+    if (upperReadAngle > angle)                                           // Extend or retract the actuator accordingly
     {
       pwm.setPWM(upperPWMPin, 0, speed);
       digitalWrite(directionUpperPin, HIGH);
@@ -155,30 +160,32 @@ void actuatorUpper(int angle, int speed)
     //Serial.println(angle);
     upperReadAngle = map(analogRead(upperPin),95,406,156,75);
   }
-  pwm.setPWM(upperPWMPin, 0, 0);
+  pwm.setPWM(upperPWMPin, 0, 0);                                    // Stop movement
 }
 
+
+//////////////////////////////////// Main Software Loop /////////////////////////////////////
 void loop()
 {
-  actuatorLower(lowerAngle,speed);
+  actuatorLower(lowerAngle,speed);                                          // Set lower actuator position
   
-  actuatorUpper(upperAngle,speed);
+  actuatorUpper(upperAngle,speed);                                          // Set upper actuator position                                
 
-  pwm.setPWM(azimuthPWMPin, 0, map(azimuthAngle,0,180,150,600));
-  delay(15);
+  pwm.setPWM(azimuthPWMPin, 0, map(azimuthAngle,0,180,SERVOMIN,SERVOMAX));  // Set servo position
+  delay(15);                                                                // ensure the servo has time to move
   
-  pwm.setPWM(horizonPWMPin, 0, map(horizonAngle,0,180,150,600));
-  delay(15); 
+  pwm.setPWM(horizonPWMPin, 0, map(horizonAngle,0,180,SERVOMIN,SERVOMAX));  // Set servo position
+  delay(15);                                                                // ensure the servo has time to move
 
-  pwm.setPWM(endservoPWMPin, 0, map(endAngle,0,180,150,600));
-  delay(15); 
+  pwm.setPWM(endservoPWMPin, 0, map(endAngle,0,180,SERVOMIN,SERVOMAX));     // Set servo position
+  delay(15);                                                                // ensure the servo has time to move
 
 
-  pwm.setPWM(spinPWMPin, 0, abs(spinSpeed));
-  digitalWrite(directionSpinPin, isPositive(spinSpeed));
+  pwm.setPWM(spinPWMPin, 0, abs(spinSpeed));                                // Set Motor PWM
+  digitalWrite(directionSpinPin, isPositive(spinSpeed));                    // Set Motor direction
 
-  pwm.setPWM(rollPWMPin,0,abs(rollSpeed));
-  digitalWrite(directionRollPin,isPositive(rollSpeed));
+  pwm.setPWM(rollPWMPin,0,abs(rollSpeed));                                  // Set Motor PWM
+  digitalWrite(directionRollPin,isPositive(rollSpeed));                     // Set Motor direction
 
   
   
