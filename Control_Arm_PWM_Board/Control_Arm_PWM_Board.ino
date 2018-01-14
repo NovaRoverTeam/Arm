0b7e27980c712f1ee16ebb55a65e12d6763f3d3d
jahai3 <35411558+jahai3@users.noreply.github.com>
2018-01-14 23:32:17 +1100

    Commented debugging serial related initialization and instructions

--- Control_Arm_PWM_Board/Control_Arm_PWM_Board.ino
+++ Control_Arm_PWM_Board/Control_Arm_PWM_Board.ino
@@ -100,7 +100,7 @@ ros::Subscriber<rover::ArmCmd> arm_sub("arm_cmd_data", &msgCallback);
 void setup()
 {
   //  setup serial
-  Serial.begin(9600);             
+  //Serial.begin(9600);             
 
   //Set pins
   pinMode(directionLowerPin, OUTPUT);
@@ -133,12 +133,12 @@ void setServoPulse(uint8_t n, double pulse) {
   
   pulselength = 1000000;   // 1,000,000 us per second
   pulselength /= 60;   // 60 Hz
-  Serial.print(pulselength); Serial.println(" us per period"); 
+  //Serial.print(pulselength); Serial.println(" us per period"); 
   pulselength /= 4096;  // 12 bits of resolution
-  Serial.print(pulselength); Serial.println(" us per bit"); 
+  //Serial.print(pulselength); Serial.println(" us per bit"); 
   pulse *= 1000;
   pulse /= pulselength;
-  Serial.println(pulse);
+  //Serial.println(pulse);
   pwm.setPWM(n, 0, pulse);
 }
 
